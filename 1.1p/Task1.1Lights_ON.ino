37b5d62883877180844c0615c382d1c97ce2ecc2
jacksonwilliamyu <williamyujackson@gmail.com>
2026-03-06 23:19:33 +1100

    Implement light control with push button
    Added functionality to control porch and hallway lights based on button press.

--- 1.1p/Task1.1Lights_ON.ino
+++ 1.1p/Task1.1Lights_ON.ino
@@ -1,1 +1,25 @@
+int pushButton = 2;
+int porchLED = 5;
+int hallwayLED = 6;
 
+void setup() {
+  pinMode(pushButton, INPUT_PULLUP);
+  pinMode(porchLED, OUTPUT);
+  pinMode(hallwayLED, OUTPUT);
+}
+
+void loop() {
+
+  if (digitalRead(pushButton) == LOW) {
+
+    digitalWrite(porchLED, HIGH);
+    digitalWrite(hallwayLED, HIGH);
+
+    delay(30000);          // porch light stays on 30 seconds
+    digitalWrite(porchLED, LOW);
+
+    delay(30000);          // hallway light stays on total 60 so just extra 60 seconds
+    digitalWrite(hallwayLED, LOW);
+  }
+
+}
